171724b47a19eab5cd96c7db9be11416a12e0966
Dmitriy Simonchik <simonchik.d@promail366.com>
2024-08-21 16:21:49 +0300

    exceptions

--- tests/Unit/GetTodosServiceTest.php
+++ tests/Unit/GetTodosServiceTest.php
@@ -3,21 +3,23 @@
 namespace App\Tests\Unit;
 
 use App\Repository\GetTodos;
-use App\Repository\GetTodosInterface;
 use App\Service\GetTodosService;
 use Codeception\Test\Unit;
 use PHPUnit\Framework\MockObject\MockObject;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
 
 class GetTodosServiceTest extends Unit
 {
     protected GetTodosService $todosService;
     protected GetTodos|MockObject $todos;
+    protected TagAwareCacheInterface|MockObject $cache;
 
 
     protected function _before()
     {
+        $this->cache = $this->createMock(TagAwareCacheInterface::class);
         $this->todos = $this->createMock(GetTodos::class);
-        $this->todosService = new GetTodosService($this->todos);
+        $this->todosService = new GetTodosService($this->todos, $this->cache);
     }
 
     public function testGetTodos()
