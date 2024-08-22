eae794ed9f5b2d5e4c09914908a2f9ff15b3009c
Dmitriy Simonchik <simonchik.d@promail366.com>
2024-08-22 16:23:13 +0300

    tests

--- tests/Unit/GetUserTodosServiceTest.php
+++ tests/Unit/GetUserTodosServiceTest.php
@@ -34,4 +34,18 @@ class GetUserTodosServiceTest extends Unit
 
         $this->assertEquals([0 => ['userId' => 1]], $this->userTodosService->getUserTodos(1));
     }
+
+    public function testGetUserTodos()
+    {
+        $this->cache->expects($this->once())
+            ->method('get')
+            ->willReturn(null);
+
+        $this->userTodos->expects($this->once())
+            ->method('get')
+            ->with(1)
+            ->willReturn([0 => ['userId' => 1]]);
+
+        $this->assertEquals([0 => ['userId' => 1]], $this->userTodosService->getUserTodos(1));
+    }
 }
